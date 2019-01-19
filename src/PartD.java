34e79c214115679165d453102453dd9b6724506d
Lwooles <lisa.wooles@hotmail.co.uk>
2019-01-20 12:40:37 +1300

    Adding the best day of the week string

--- src/PartD.java
+++ src/PartD.java
@@ -5,6 +5,7 @@ public class PartD {
 		System.out.println("Hi, my name is Lisa");
 		System.out.println("My favourite colour is orange");
 		System.out.println("My favourite mode of transportation is car");
+		System.out.println("The best day of the week is Saturday");
 
 	}
 
