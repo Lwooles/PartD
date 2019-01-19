a9fdd6497a3ab270f60c239c20374d120b98b02f
Lwooles <lisa.wooles@hotmail.co.uk>
2019-01-20 12:39:46 +1300

    Adding favourite transport string

--- src/PartD.java
+++ src/PartD.java
@@ -4,6 +4,7 @@ public class PartD {
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Lisa");
 		System.out.println("My favourite colour is orange");
+		System.out.println("My favourite mode of transportation is car");
 
 	}
 
